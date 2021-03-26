b45e29c94aa17a0624d9b6ee6aa7f74d6b02ae52
farad233 <60288584+farad233@users.noreply.github.com>
2021-03-26 22:07:49 +0200

    timer

--- js/funny.js
+++ js/funny.js
@@ -2,11 +2,19 @@ var sdvig = 0;
 var test = document.getElementById('logo_move');
 var logoMove = document.getElementById('tittle_company_move');
 move()
-function move() {
+function move(time) {
     test.style.transform = 'rotateY(' + sdvig + 'deg)';
     test.style.transition = 5 + 's' + ' cubic-bezier(.47,.57,.13,.69)';
     sdvig = sdvig + 360;
+    time = sdvig;
     setTimeout(move, 5200);
 }
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState === 'visible') {
+    move(0);
+  } else {
+    move(0);
+  }
+});
 
 
